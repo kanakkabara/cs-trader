79b84267a0710f342de66212cd0e8428cfe35a4b
Kanak Kabara <kanakkabara@hotmail.com>
2018-08-13 21:32:55 -0400

    init

---
+++ src/main/java/com/cs/trader/CsTraderApplication.java
@@ -0,0 +1,12 @@
+package com.cs.trader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsTraderApplication.class, args);
+	}
+}
