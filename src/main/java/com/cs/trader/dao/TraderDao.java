8da070517f3275e8f76059823d6318568d81af61
Student <Student@ROIWinVM.localdomain>
2018-08-13 23:34:30 -0400

    - Added TraderDaoTest (incomplete) - Edited traderdao list/delete functions

--- src/main/java/com/cs/trader/dao/TraderDao.java
+++ src/main/java/com/cs/trader/dao/TraderDao.java
@@ -1,9 +1,12 @@
 package com.cs.trader.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.cs.trader.domain.Trader;
 
@@ -13,7 +16,7 @@ public class TraderDao {
 	private JdbcTemplate jdbcTemplate;
 	
 	public int addTrader(Trader trader) {
-		String sql = "insert into TRADERS (firstName, lastName, email, phone, address) values(?,?,?,?,?)";
+		String sql = "INSERT INTO TRADERS (FIRST_NAME, LAST_NAME, EMAIL, PHONE, ADDRESS) VALUES(?,?,?,?,?)";
 		int status = jdbcTemplate.update(sql,
 				new Object[] { trader.getFirstName(), trader.getLastName(), trader.getEmail(), 
 						trader.getPhone(), trader.getAddress()});
@@ -21,15 +24,34 @@ public class TraderDao {
 	}
 	
 	public int deleteTrader(int id) {
-		return -1;
+		String sql = "DELETE FROM TRADERS WHERE TRADER_ID = ?";
+		int status = jdbcTemplate.update(sql, new Object[] {id});
+		return status;
 	}
 	
 	public List<Trader> findTraders(){
-		return null;
+		return jdbcTemplate.query("select * from users",
+				new TraderRowMapper());
 	}
 	
 	public Trader findTraderById(int id) {
-		return null;
+		String sql = "SELECT * FROM TRADERS WHERE TRADER_ID = ?";
+		return jdbcTemplate.queryForObject(sql,
+				new TraderRowMapper(), id);
+	}
+	
+	class TraderRowMapper implements RowMapper<Trader>{
+		@Override
+		public Trader mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Trader trader = new Trader();
+			trader.setTraderId(rs.getInt("TRADER_ID"));
+			trader.setFirstName(rs.getString("FIRST_NAME"));
+			trader.setLastName(rs.getString("LAST_NAME"));
+			trader.setEmail(rs.getString("EMAIL"));
+			trader.setPhone(rs.getString("PHONE"));
+			trader.setAddress(rs.getString("ADDRESS"));
+			return trader;
+		}
 	}
 	
 	
